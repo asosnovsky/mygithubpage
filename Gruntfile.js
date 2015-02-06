f22baee56547771859e389efad689f9a061e120e
Ari Sosnovsky <info@asosnovsky.me>
2015-02-06 16:29:38 -0600

    fixed a whole lotta stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,9 +411,8 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve:' + target]);
+  grunt.registerTask('server', 'starting dist', function (target) {
+    return grunt.task.run(['connect:dist:keepalive']);
   });
 
   grunt.registerTask('test', [
